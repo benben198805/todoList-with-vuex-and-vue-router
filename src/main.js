f27de3c335ed53ddca73c6fc42f2eac58f2039a5
qlyan(benben) <qlyan@thoughtworks.com>
2017-02-08 22:40:21 +0800

    [#010][Yan Qilong] add element-ui in vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import App from './App';
 import Vuex from 'vuex';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css'
 import router from './routers';
 import store from './vuex/index';
 
 Vue.use(Vuex);
+Vue.use(ElementUI);
+
 new Vue({
     el: '#app',
     store: store,
