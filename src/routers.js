4a4294601aaecba994e17ae30d19d6a3bf5373e3
qlyan(benben) <qlyan@thoughtworks.com>
2017-02-10 17:14:56 +0800

    [#014][Yan Qilong] modify router

--- src/routers.js
+++ src/routers.js
@@ -3,22 +3,18 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 const routes = [{
-    path: '/#/',
-    component: require('./App'),
-    children: [{
-        path: '/home',
-        name: 'home',
-        component: require('./pages/home')
-    },{
-        path: '/history',
-        name: 'history',
-        component: require('./pages/history.vue')
-    }]
+    path: '/home',
+    name: 'home',
+    component: require('./pages/home')
+}, {
+    path: '/history',
+    name: 'history',
+    component: require('./pages/history.vue')
 }];
 
 const router = new VueRouter({
     routes: routes,
-    history:true
+    history: true
 });
 
 export default router
